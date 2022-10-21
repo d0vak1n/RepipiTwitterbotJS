7be7cb0df60cc45344ef5330ff6321e3865b1833
Raul <rulanxa@gmail.com>
2022-10-21 03:30:37 +0200

    get twits con la palabra gi*******s

--- bot.js
+++ bot.js
@@ -1,2 +1,7 @@
-var twit = require('twit');
-var config = require('./config.js');
+var Twit = require('twit')
+const auth = require("./config")
+
+ 
+auth.T.get('search/tweets', { q: 'gilipollas since:2011-07-11', count: 10 }, function(err, data, response) {
+  console.log(data)
+})
