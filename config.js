0be513b9f208108dd23b22b9d59491f87bd12a0b
Raul <rulanxa@gmail.com>
2022-10-24 12:20:47 +0200

    bugfix

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ var T = new Twit({
   consumer_secret:      'BS7tdDQHR2W7gYGruYmeOKufhbSz2k3qfGAkME6LjOsOasEcjT',
   access_token:         '1583402583502241793-tZWPfBrdVdgiIMNxZpf8FTz29Apxir',
   access_token_secret:  'C5fHxdnmoYqvkUvRgAqO7CPjmENPXt2AnMyvcxP3kT41q',
-  timeout_ms:           60*60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            false,     // optional - requires SSL certificates to be valid.
+  timeout_ms:           60*60*1000,  // optional HTTP request timeout to apply to all requests
+  strictSSL:            false,     // optional - requires SSL certificates to be valid
 })
 
 module.exports = { T };
