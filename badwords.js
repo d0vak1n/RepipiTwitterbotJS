b7633b338e1c36fcb9bee98b7e7e9a6921e3c05e
Raul <rulanxa@gmail.com>
2022-10-24 10:48:47 +0200

    showing 5 tweets with badword

--- badwords.js
+++ badwords.js
@@ -1,8 +1,45 @@
 var fs = require('fs');
+const Twit = require('twit');
+const obj = require('./tweetdata')
+var arraybadwords = getDataSet("ESP");
+var intervalo = 10 * 1000;
+const auth = require('./config');
+const { stringify } = require('querystring');
+var wordofhour;
 
-function getDataSet(filelang){
-    arraypal = fs.readFileSync('./dataset/'+filelang+'.txt').toString().split("\n");
-    return arraypal;
+module.exports = { getDataSet, twitearPalabra, wordofhour };
+
+function getDataSet(filelang) {
+  arraypal = fs.readFileSync('./dataset/' + filelang + '.txt').toString().split("\n");
+  return arraypal;
 }
 
-module.exports = { getDataSet };
+
+function twitearPalabra() {
+  console.log("Se twitea");
+  var aleat = Math.round(Math.random() * arraybadwords.length - 1);
+  wordofhour = arraybadwords[aleat];
+
+  auth.T.post(
+    'statuses/update', {
+    status: 'En la hora que viene, te declaro une: ' + wordofhour
+  },
+    function (err, data, response) {
+      data => { }
+      //console.log(data);
+      console.log("Twit creado con la palabra: " + wordofhour);
+      console.log("Esperando " + intervalo / 1000 + " segundos");
+    })
+
+  auth.T.get('search/tweets', { q: '' + wordofhour + ' since:2011-07-11', count: 5 }, function (err, data, response) {
+    const datos = data;
+    let i = 0;
+    while (i < datos.statuses.length) {
+      console.log(datos.statuses[i].text);
+      i++;
+    }
+  })
+
+}
+
+setInterval(twitearPalabra, intervalo);
--- bot.js
+++ bot.js
@@ -1,24 +1,9 @@
 var Twit = require('twit');
-const auth = require('./config');
-const bdwds = require('./badwords');
+//const auth = require('./config');
+//const bdwds = require('./badwords');
+const { fork } = require('child_process');
 
-var arraybadwords = bdwds.getDataSet("ESP");
+
+fork("./badwords.js");
 
 console.log("Bot iniciado, indexando insultos en idioma español :)");
-
-function twitearPalabra() {
-
-  var aleat = Math.round(Math.random()*arraybadwords.length-1);
-  var wordofhour = arraybadwords[aleat];
-
-  auth.T.post(
-    'statuses/update', {
-    status: 'El insulto al que le debemos una ovación es: ' + wordofhour
-  },
-    function (err, data, response) {
-      data => { }
-      console.log(data);
-    })
-}
-
-setInterval(twitearPalabra, 60*60*1000);
