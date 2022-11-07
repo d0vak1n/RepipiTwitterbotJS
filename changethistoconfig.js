97b996d678c4145db0f315920859bf11b177453d
Raul <rulanxa@gmail.com>
2022-11-07 23:26:55 +0100

    updated config file

--- changethistoconfig.js
+++ changethistoconfig.js
@@ -6,15 +6,16 @@
 // ╚██████╗╚██████╔╝██║ ╚████║██║     ██║╚██████╔╝
 //  ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝ 
 // Change this file name to config.js
-var Twit = require('twit')
+var Twit = require('twit');
 
+var intervalo = 60/* Minutes */ * 60 /* Seconds */ * 1000;
 var T = new Twit({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token: '',
-  access_token_secret: '',
-  timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL: false,     // optional - requires SSL certificates to be valid.
+  consumer_key:         '---',
+  consumer_secret:      '---',
+  access_token:         '---',
+  access_token_secret:  '---',
+  timeout_ms:           60*60*1000,  // optional HTTP request timeout to apply to all requests
+  strictSSL:            false,     // optional - requires SSL certificates to be valid
 })
 
-module.exports = { T }
+module.exports = { T ,intervalo };
