b18bf51ceef22d6e009062e1b74dafc91d8aec75
d0vAk1N <raul@ramoweb.com>
2023-07-31 17:32:18 +0200

    started migration to twitter-api-v2

---
+++ src/job.ts
@@ -0,0 +1,23 @@
+import auth from "../config";
+import fs from "fs";
+import TwitterApi from "twitter-api-v2";
+
+function getLang() {
+    let arrayLang = ["ESP"];
+    let aleat = Math.round(Math.random() * (arrayLang.length - 1));
+    let lang = arrayLang[aleat];
+    console.log("We decided to use " + lang);
+    return lang;
+  }
+
+  function getDataSet(filelang: any) {
+    let arraypal = fs
+      .readFileSync("./dataset/" + filelang + ".txt")
+      .toString()
+      .split("\n");
+    return arraypal;
+  }
+
+  const twitterRepipi = new TwitterApi('1583402583502241793-ksq9tGBt6gUoQJDtWZTgfL0bm5jq26');
+  
+  twitterRepipi.v1.uploadMedia('./GSSlRLHQ_400x400.jpg');
