110b23247ef48758bdc05f9e7e56d5326e03868d
Raul <rulanxa@gmail.com>
2023-10-17 01:04:57 +0200

    working simple auth method knowing the secret KEY

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import {twitAndRetwitWord} from './job';
-
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import { twitAndRetwitWord } from "./job";
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
-app.get('/insultar/:auth?', (req: Request, res: Response) => {
-    const auth = req.params.auth;
+app.get("/insultar/:auth?", async (req: Request, res: Response) => {
+  const auth = req.params.auth;
 
-    if (auth == `${process.env.API_BOT_KEY}`) {
-        twitAndRetwitWord();
-        res.send('Éxito!!');
-    }
-
-
- twitAndRetwitWord();
-
+  if (auth == `${process.env.API_BOT_KEY}`) {
+    const response = await twitAndRetwitWord();
+    res.status(200).json({ response });
+  } else {
+    console.log("Alguien esta intentando acceder Ò.Ó");
+    res.status(401).json({ message: "Unauthorized" });
+  }
 });
 
 app.listen(port, () => {
